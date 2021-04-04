d5af5c2b099b61d6d0d27d075ee056bd6d151fbe
sezaite <martyna.sezaite@gmail.com>
2021-04-04 20:37:19 +0100

    pseudo kodas

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-const fetch = require("node-fetch");
 async function getData() {
     const response = await fetch('https://www.wix.com/_serverless/hiring-task-spreadsheet-evaluator/jobs');
     return response.json();
 }
-const fs = require('fs');
-getData()
-    .then(data => {
-        fs.writeFileSync('student-2.json', data);
-        console.log('The file has been saved!');
-    });
 
+const data = await getData();
+
+const jobs = data['jobs'];
+console.log(jobs);
+
+for (let i = 0; i < jobs.length; i++) {
+    mapinti per jobs[i]['data']
+    jei value, return value, jei formule, paleidziam f -, kuri tikrins formule
+}
+}
+
+formules tikrinimas:
+tikrini, koks veiksmas
+tikrinti, ar referensas ar value
+apsirasyti nesuderinamumus
